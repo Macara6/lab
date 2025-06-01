7b951de2ac02bf55f66421f171bac886f3c87948
Rmacara <151909529+Macara6@users.noreply.github.com>
2025-06-01 02:53:28 +0200

    Update settings.py

--- gestion/settings.py
+++ gestion/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+            "http://localhost:5173/",
            "https://bilatech.vercel.app/",
           # Adjust this to match your Vue.js app's URL
        ]
