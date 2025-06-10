ed1c9e32da827f89121f695d68678513f51d3869
Reginald macara <reginadmacara16@gmail.com>
2025-06-10 22:42:46 +0200

    Mise à jour du code depuis la machine locale

--- Myapp/models.py
+++ Myapp/models.py
@@ -1,10 +1,17 @@
+
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User , AbstractUser# type: ignore
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    entrep_name = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    adress= models.CharField(max_length=40, null=True, blank=True)
+    rccm_number= models.CharField(max_length=40)
+    impot_number= models.CharField(max_length=255)
+
     
 class Category(models.Model):
 
@@ -14,7 +21,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
 
 class Product(models.Model):
    name = models.CharField(max_length=50)
@@ -39,3 +45,55 @@ class Invoice(models.Model):
     def __str__(self):
         created_at= self.created_at.strftime('%Y-%m-%d %H:%M')
         return f"Invoice {self.id} - {self.client_name} -{created_at}"
+    
+
+# class pour geree les adonnnemnt des utilisateur  
+ 
+class Subscription(models.Model):
+    BASIC = 'BASIC'
+    MEDIUM = 'MEDIUM'
+    PREMIUM = 'PREMIUM'
+
+    SUBSCRIPTION_TYPES = [
+        (BASIC, 'Basic'),
+        (MEDIUM, 'Medium'),
+        (PREMIUM, 'Premium'),
+    ] 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription_type = models.CharField(max_length=10, choices=SUBSCRIPTION_TYPES,default=BASIC)
+    
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateTimeField(auto_now=True)
+    end_date =models.DateTimeField()
+    is_active =models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"subscription forn {self.user.username} - Activate: {self.is_active} "
+    
+    def is_expired(self):
+        
+        return self.end_date <= timezone.now()
+    
+    def deactivate_subscription(self):
+        self.is_active = False
+        self.save()
+
+class CashOut(models.Model):
+    user =  models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now= True)
+    motif = models.CharField(max_length=30, default="Aucun motif")
+    def __str__(self):
+        return f"{self.user.username} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+    
+    @property
+    def total_amount(self):
+        return sum(detail.amount for detail in self.details.all())
+
+class CashOutDetail(models.Model):
+    cashout = models.ForeignKey(CashOut, related_name='details', on_delete=models.CASCADE)
+    reason = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+       return f"{self.reason} - {self.amount}"   
+
