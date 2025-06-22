7fd8b2c2871482e24137af78c46ea49f1ac346e5
Rmacara <151909529+Macara6@users.noreply.github.com>
2025-06-22 13:45:44 +0200

    Update settings.py

--- gestion/settings.py
+++ gestion/settings.py
@@ -140,7 +140,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gestion',
         'USER': 'adminuser',
-        'PASSWORD':'AdminPass123@',
+        'PASSWORD':'BilaTech@25',
         'HOST': 'localhost',
         'PORT': '3306',
     }
