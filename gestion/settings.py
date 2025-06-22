051d09768d96337ce1986cf81256799a6dcc2a8b
Rmacara <151909529+Macara6@users.noreply.github.com>
2025-06-22 11:43:35 +0200

    Update settings.py

--- gestion/settings.py
+++ gestion/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-cr+nt5y*e83_0+6&xopwjgvc9pi2ul&dys(q74o9uu=syp5q8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.193.20.200','*']
+ALLOWED_HOSTS = ['api.bilatech.org','*']
 
 
 SIMPLE_JWT = {
