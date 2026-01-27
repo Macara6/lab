a3cd2092f0621cfa5daad1b517414621599b9dd1
Reginald macara <reginadmacara16@gmail.com>
2026-01-27 04:39:47 +0100

    Remove ignored files from repository

--- gestion/urls.py
+++ gestion/urls.py
@@ -30,7 +30,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-#if settings.DEBUG:
+if settings.CORS_ALLOW_ALL_ORIGINS:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
  #   urlpatterns += [
   #      path('silk/', include('silk.urls', namespace='silk')),
